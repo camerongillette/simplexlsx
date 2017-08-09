08ddda9d50c3978f65e19384658430377d86ab5c
camerongillette <camerongillette@gmail.com>
2017-08-09 16:01:00 -0400

    Add files via upload
    This example handles the usecase where worksheet id's don't start at 1.
    This code will dynamically detect all worksheet id's and show the contents of all worksheets indepedent of what the worksheet id's are.

---
+++ simplexlsx.example5.php
@@ -0,0 +1,32 @@
+<?php
+include_once('header.php');
+global $ERROR, $user;
+
+$filepath = $_GET['filepath'];
+
+$xlsx = new SimpleXLSX('uploads/countries_and_population.xlsx',false,true);
+
+//Get all worksheet id's
+$worksheetIds = [];
+$allSheets = $xlsx->sheets();
+while ($currentSheet = current($allSheets)) {
+    array_push($worksheetIds,key($allSheets));
+    next($allSheets);
+}
+
+echo '<table cellpadding="10">';
+foreach($worksheetIds as $worksheetId){
+    echo '<tr><td valign="top">';
+    list($num_cols, $num_rows) = $xlsx->dimension($worksheetId);
+    echo '<h1>Sheet ' . $worksheetId . '</h1>';
+    echo '<table>';
+    foreach( $xlsx->rows($worksheetId) as $r ) {
+            echo '<tr>';
+            for( $i=0; $i < $num_cols; $i++ )
+                    echo '<td>'.( (!empty($r[$i])) ? $r[$i] : '&nbsp;' ).'</td>';
+            echo '</tr>';
+    }
+    echo '</table></td></tr>';
+}
+echo '</table>';
+?>
